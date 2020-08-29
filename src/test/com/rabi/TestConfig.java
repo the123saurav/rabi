f261b15307b38e02bdc4f049ab6aeba2417a50bf
the123saurav <42906612+the123saurav@users.noreply.github.com>
2020-08-29 22:12:42 +0530

    Amend test directory for maven (#7)
    Move tests under src/test/java for maven to find unit tests

--- src/test/com/rabi/TestConfig.java
+++
@@ -1,6 +0,0 @@
-package com.rabi;
-
-public class TestConfig {
-  public static final String LOGGER_NAME = "rabi-test-log";
-  public static final int OPEN_TIMEOUT_SEC = 10;
-}
---
+++ src/test/java/com/rabi/TestConfig.java
@@ -0,0 +1,6 @@
+package com.rabi;
+
+class TestConfig {
+  static final String LOGGER_NAME = "rabi-test-log";
+  static final int OPEN_TIMEOUT_SEC = 10;
+}
